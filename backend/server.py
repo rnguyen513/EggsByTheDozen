d82731676a901661819464da4daa9f272492c2eb
Shaunak <shaunakduude@gmail.com>
2024-04-18 11:34:04 -0400

    testing api

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from inference_sdk import InferenceHTTPClient
+from fastapi.responses import FileResponse
+from fastapi import UploadFile
 
 app = FastAPI()
 
@@ -14,3 +17,17 @@ app.add_middleware(
 @app.get("/")
 def home():
     return {"Data": "Test"}
+
+@app.post("/upload-images")
+async def upload_images(file_upload: UploadFile):
+   
+   contents = await file_upload.read()
+   
+   CLIENT = InferenceHTTPClient(
+    api_url="https://detect.roboflow.com",
+    api_key="bFXj3Gxls3mCRVqByeuO"
+    )
+
+   result = CLIENT.infer(contents, model_id="ovos-de-parasitas-azoug/6")
+   return result 
+

44896454fb8f395a1810c97ef45744801737cbb8
Shaunak <shaunakduude@gmail.com>
2024-04-23 12:13:56 -0400

    Added frontend and backend integration

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,10 +3,14 @@ import eggz from "../eggs.jpg"
 
 export const ImageForm = () => {
 
-    const [firstImage, setFirstImage] = useState(null);
-    const [secondImage, setSecondImage] = useState(null);
+    const endpoint_calculation = "http://localhost:8000/upload-images"
+
+    const [file_upload, setFirstImage] = useState(null);
+    const [file_upload2, setSecondImage] = useState(null);
+    const [severity, setSeverity] = useState(null);
   
     const [eggNo, setEggNo] = useState(null);
+    const [loading, setLoading] = useState(null);
   
     const handleFirstImageChange = (event) => {
       setFirstImage(event.target.files[0]);
@@ -16,23 +20,41 @@ export const ImageForm = () => {
       setSecondImage(event.target.files[0]);
     };
   
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
       event.preventDefault();
+      setLoading(true)
   
       const formData = new FormData();
-      formData.append('firstImage', firstImage);
-      formData.append('secondImage', secondImage);
+      formData.append('file_upload', file_upload);
+      formData.append('file_upload2', file_upload2);
   
       //post to server maybe????
-      fetch('/upload-images', {
+      const response = await fetch(endpoint_calculation, {
         method: 'POST',
         body: formData,
       })
-      .then(response => response.json())
-      .then(data => console.log('Success:', data))
-      .catch((error) => console.error('Error:', error));
-  
-      setEggNo(Math.floor(Math.random()*99)+1);
+      
+      if(response.ok){
+        console.log("Success")
+        console.log(response)
+        const response_data = await response.json()
+        const average_num = response_data.Average
+        setEggNo(average_num);
+        if (average_num >= 250 && average_num < 400){
+          setSeverity("Mild infection");
+        } else if (average_num >= 400 && average_num < 750){
+          setSeverity("Moderate infection");
+        } else if (average_num >= 750){
+          setSeverity("Severe infection");
+        } else {
+          setSeverity("Low Risk of infection");
+        }
+        
+      } else {
+        console.error("No video selected")
+      }
+
+      setLoading(false)
     };
   
     return (
@@ -62,7 +84,8 @@ export const ImageForm = () => {
             className="bg-red-200/10 rounded-lg hover:text-blue-300 hover:cursor-pointer"
           />
         </form>
-        <h1>Result: {eggNo ? (<b className="font-bold text-5xl text-red-400">{eggNo} eggs</b>):(null)}</h1>
+        <h1>McMaster Score: {loading ? "Loading...": (eggNo ? <b className="font-bold text-5xl text-red-400">{eggNo}</b> :null)}</h1>
+        <h1>Severity: {loading ? "Loading...": (severity ? <b className="font-bold text-5xl text-red-400">{severity}</b> :null)}</h1>
       </div>
     );
   };
